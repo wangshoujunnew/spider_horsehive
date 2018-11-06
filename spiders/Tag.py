9d04f3242f7d46036c74db4cf472af5165b6bcc1
shoujunw <shoujunw@tujia.com>
2018-11-06 14:36:26 +0800

    mysqldb修改

--- spiders/Tag.py
+++ spiders/Tag.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 import math
-import scrapy
 from horsehivecomment.items import *
-from scrapy.http.request.form import Request
-from scrapy.http.response.html import HtmlResponse
-from scrapy.selector import Selector
 from horsehivecomment.utils.Utils import *
 import json
 import re
+
+
 # 爬取酒店详情页下面 每个6个topic 项目对应的评论链接
 class TagSpider(scrapy.Spider):
     name = 'Tag'
@@ -29,13 +27,13 @@ class TagSpider(scrapy.Spider):
     def parse(self, response):  # 得到酒店详情页的6个标签
         for tag in response.xpath('//a[@class="_j_comment_keyword"]'):  # **
             item = TagItem()
-
-            item['dataId'] = tag.xpath('@data-id').extract_first()  # *
-            item['hotelId'] = re.findall('hotel/(\d+).html', response.url)[0]  # *
-            item['dataType'] = tag.xpath('@data-type').extract_first()  # *
-            item['num'] = tag.xpath('i/text()').extract_first()
-            item['totalNum'] = tag.xpath('span/em/text()').extract_first()  # * 这个标签总点评量 / 10,可以得到页数
-            item['tagName'] = tag.xpath('strong/text()').extract_first()  # *
+            tmpJson = {}
+            tmpJson['dataId'] = tag.xpath('@data-id').extract_first()  # *
+            tmpJson['hotelId'] = re.findall('hotel/(\d+).html', response.url)[0]  # *
+            tmpJson['dataType'] = tag.xpath('@data-type').extract_first()  # *
+            tmpJson['num'] = tag.xpath('i/text()').extract_first()
+            tmpJson['totalNum'] = tag.xpath('span/em/text()').extract_first()  # * 这个标签总点评量 / 10,可以得到页数
+            tmpJson['tagName'] = tag.xpath('strong/text()').extract_first()  # *
             print(">>>>>>>>>>")
             # 生成url
             # var
@@ -66,10 +64,7 @@ class TagSpider(scrapy.Spider):
                                 return JSON.stringify(B)
                                 ''' % (item['hotelId'], str(0), item['dataId'], str(page))  # 先默认要一个page = 1的
                     print('执行js>>>>')
-                    # print(script)
-
-                    # poi_id=40046&type=0&keyword_id=0&page=2
-                    result = utils.getJsEnvironment( script=script) # url=jsurl,
+                    result = utils.getJsEnvironment(script=script)  # url=jsurl,
 
                     parameter = json.loads(result)
                     writeUrl = ''
@@ -81,11 +76,11 @@ class TagSpider(scrapy.Spider):
                             writeUrl += key + '=' + str(parameter[key]) + '&'
 
                     writeUrl = commentListUrl + writeUrl
-                    writeData = {'tagName': item['tagName'], 'commentListUrl': writeUrl}
 
-                    # utils.writeToFile('commentListUrl.txt',
-                    #                  text=json.dumps(writeData).encode('utf-8').decode('unicode_escape'), append=True)
-                    yield item # 将写入文件的方式改成写入到mysql中
+                    item = TagItem()
+                    item['tagName'] = tmpJson['tagName']
+                    item['commentListUrl'] = writeUrl
+                    yield item  #
             else:
                 pass
 
--- utils/MysqlDB.py
+++ utils/MysqlDB.py
@@ -1,36 +1,40 @@
+#encoding=utf-8
 import MySQLdb
 # mysql 连接工具
 
 class MysqlDB:
     def __init__(self):
-        self.host = ""
-        self.dbname = ""
-        self.user = ""
-        self.password = ""
-        self.charcode = "utf-8"
-        self.db = MySQLdb.connect(self.host,
-                                  self.user,
-                                  self.password,
-                                  self.dbname,
-                                  self.charcode)
+        self.host = "localhost"
+        self.port = 3306
+        self.dbname = "horsehive"
+        self.user = "root"
+        self.password = "wsjia369X"
+        self.charcode = "utf8"
+        self.db = MySQLdb.connect(host = self.host,
+                                  port = self.port,
+                                  user = self.user,
+                                  passwd = self.password,
+                                  db = self.dbname,
+                                  charset = self.charcode)
 
     # 解析item, 插入item数据到mysql中
-    def insert(self, item):
+    def insert(self, item, tb):
         try:
             names = ""
             values = ""
             for key in item.keys():
                 names += key + ','
-                values += item[key] + ','
+                values += '"{value}"'.format(value = item[key]) + ','
             names = names.rstrip(',')
             values = values.rstrip(',')
 
-            sql = "insert into table({names}) values({values})".format(names=names, values=values)
-
+            sql = "insert into {tb}({names}) values({values})".format(names=names, values=values, tb=tb)
+            # print(sql)
             cursor = self.db.cursor()
             cursor.execute(sql)
             self.db.commit()
         except:
+            print('insert raise error')
             self.db.rollback()
 
     def close(self):
