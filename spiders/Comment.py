b3b0afa82189f1b54e7c76f507f96a7fd57525d8
wangshoujunnew <37709822+wangshoujunnew@users.noreply.github.com>
2019-03-02 13:08:09 +0800

    Update Comment.py

--- spiders/Comment.py
+++ spiders/Comment.py
@@ -15,7 +15,8 @@ class CommentSpider(scrapy.Spider):
     urlData = []
     # 初始化 , 从数据库中拿出10个来做爬虫
     mysql = MysqlDB()
-
+    
+    # 提取没有爬的URL
     def start_requests(self):
         while (True):
             if CommentSpider.urlData.__len__() <= 0:
@@ -38,7 +39,8 @@ class CommentSpider(scrapy.Spider):
 
     def parse(self, response):
         if not requestError.error(response):  # 请求状态为200
-            html = (json.loads(response.text))['html']
+            html = (json.loads(response.text))['html'] # 建议使用pyquery选择
+            
             for element in scrapy.Selector(text=html).xpath(
                     '//div[@class="comm-item _j_comment_item"]'):
                 item = CommentItem()
