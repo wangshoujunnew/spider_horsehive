9020c152b12c6835da5d495ce3c6954ceb488052
wangshoujunnew <37709822+wangshoujunnew@users.noreply.github.com>
2019-03-02 13:19:28 +0800

    Update Base.py

--- spiders/Base.py
+++ spiders/Base.py
@@ -4,51 +4,36 @@ import json
 import scrapy
 from horsehivecomment.utils.Utils import *
 import copy
-
+# 模版
 
 # 解析每个评论链接,得到评论内容,写入到mysql中
 
-class CommentSpider(scrapy.Spider):
+class BaseSpider(scrapy.Spider):
     name = 'Comment'
     allowed_domains = ['www.mafengwo.cn']
-    commentListUrlDict = {}  # 存储url对应的tag_name
-    urlData = []
+    start_url = []
+    has_spider_url = []
     # 初始化 , 从数据库中拿出10个来做爬虫
     mysql = MysqlDB()
     
     # 提取没有爬的URL
     def start_requests(self):
         while (True):
-            if CommentSpider.urlData.__len__() <= 0:
-                CommentSpider.urlData = mysql.select('select * from tag_2 where flag = 0 limit 100',
+            if len(BaseSpider.start_url) <= 0:    		# 数据库中或者文件中取出没有爬取的url放入到start_url中, 每次爬取100个url
+            											# 最好改成文件中, 比较方便
+                BaseSpider.start_url = mysql.select('select * from tag_2 where flag = 0 limit 100',
                                                      {'id': '', 'tag_name': '', 'hotel_id': '', 'comment_urlpage': '',
                                                       'flag': ''})
-                if not CommentSpider.urlData:
-                    break
 
-                for line in CommentSpider.urlData:
-                    CommentSpider.commentListUrlDict[line['comment_urlpage']] = line['tag_name']
-
-                for url in copy.deepcopy(CommentSpider.urlData):
-                    yield scrapy.Request(url=url['comment_urlpage'], callback=self.parse)
-                    tmpurl = {}
-                    tmpurl['id'] = url['id']
-                    tmpurl['flag'] = '1'
-                    mysql.update(tmpurl, 'tag_2')
-                    CommentSpider.urlData.remove(url)
+                for url in copy.deepcopy(BaseSpider.start_url):
+                    yield scrapy.Request(url=url, callback=self.parse) # 请求url的内容
+                    has_spider_url.append(url)          # 将url添加到已经爬取的队列中
 
     def parse(self, response):
-        if not requestError.error(response):  # 请求状态为200
-            html = (json.loads(response.text))['html'] # 建议使用pyquery选择
-            
-            for element in scrapy.Selector(text=html).xpath(
-                    '//div[@class="comm-item _j_comment_item"]'):
-                item = CommentItem()
-                item['content'] = element.xpath('div[@class="txt"]/text()').extract_first()
-                item['star'] = element.xpath('div[@class="comm-meta"]/span/@class').extract_first()
-                item['star'] = item['star'][-1]
-                item['tag_name'] = CommentSpider.commentListUrlDict[response.url]
-                yield item
+        if response.status == 200:                      # 请求状态为200
+            html = (json.loads(response.text))['html']  # 建议使用pyquery选择
+            item = Item()
+            item['?'] = 0
+            yield item
         else:
             print('请求失败')
-
